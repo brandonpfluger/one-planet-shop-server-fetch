3275a066d48b88fd09df27f0d23820699b36bbe5
Brandon Pfluger <bkpfluger@gmail.com>
2022-02-26 17:10:52 +0100

    set up react environment for server stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products, setProducts] = useState([]);
+    const [blogPosts, setBlogPosts] = useState([]);
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+      fetch('http://localhost:7070/products')
+      .then((res) => res.json())
+      .then((res) => setProducts(res));
+    },[])
+
+    useEffect(() => {
+      fetch('http://localhost:7070/blogposts')
+      .then((res) => res.json())
+      .then((res) => setBlogPosts(res));
+    },[])
+
+    useEffect(() => {
+      fetch('http://localhost:7070/reviews')
+      .then((res) => res.json())
+      .then((res) => setReviews(res));
+    },[])
+
+
+    return (
+      <>
+      <div>
+        {products.map((product, index) => 
+        (
+          <>
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+            <p>{product.weight}</p>
+          </>
+        ))}
+      </div>
+      <div className="App">
+      {blogPosts.map((post, index) =>
+        (<>
+          <h1>{post.blogTitle}</h1>
+        <p>{post.blogText}</p>
+        </>
+        ))
+      }
+      </div>
+      <div>
+        {reviews.map((review, index) =>
+        (
+          <>
+          <h2>{review.headline}</h2>
+          <p>{review.review}</p>
+          <p>{review.rating}</p>
+          </>
+        ))}
+      </div>
+      </>
+    );
 }
 
 export default App;
+
+// {latestStories.map((story) => (
+//   <li>
+//     <a href={story.url}>{story.title}</a>
+//     <br />
+//     Author: {story.author}
+//   </li>
+// ))}
