e311d22792308dba3745efad89ede54e8f23c2dc
Tamara Ilic <tamara@ilicmcfarlane.io>
2022-03-05 09:48:04 +0100

    add main app styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+import './styles/App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
